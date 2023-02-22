b1b403eff94b5e5c8b8c9bf15d24173d697f7b62
r.ziafaty <r.ziafaty@hotmail.com>
2023-02-22 20:06:53 +0330

    encrypt examples

--- example/examples.go
+++ example/examples.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"reflect"
 
@@ -12,6 +13,8 @@ func main() {
 	marshalUnmarshalExample()
 	encodeDecodeExample()
 	marshalUnmarshalCompressExample()
+	marshalUnmarshalEncryptExample()
+	marshalUnmarshalCompressEncryptExample()
 }
 
 // marshal src and unmarshel the returned data to dst
@@ -57,6 +60,54 @@ func marshalUnmarshalCompressExample() {
 
 	data := gotiny.MarshalCompress(&src1, &src2)
 	gotiny.UnmarshalCompress(data, &dst1, &dst2)
+
+	fmt.Println(dst1 + string(dst2)) // print "hello world!"
+}
+
+// marshal src and unmarshel the returned data to dst
+// with compression and encryption
+func marshalUnmarshalCompressEncryptExample() {
+	src1, src2 := "marshalUnmarshalCompressEncrypt", []byte(" Example!")
+	dst1, dst2 := "", []byte{3, 4, 5}
+
+	var str = "0123456789abcdef0123456789abcdef" // 32-byte hex string
+	var key [32]byte
+
+	// Convert the string to a byte slice
+	bSlice, err := hex.DecodeString(str)
+	if err != nil {
+		panic(err)
+	}
+
+	// Copy the byte slice into the array
+	copy(key[:], bSlice)
+	aesConfig := gotiny.NewAES256config(key)
+	data := gotiny.MarshalCompressEncrypt(aesConfig, &src1, &src2)
+	gotiny.UnmarshalCompressEncrypt(aesConfig, data, &dst1, &dst2)
+
+	fmt.Println(dst1 + string(dst2)) // print "hello world!"
+}
+
+// marshal src and unmarshel the returned data to dst
+// with compression and encryption
+func marshalUnmarshalEncryptExample() {
+	src1, src2 := "marshalUnmarshalEncrypt", []byte(" Example!")
+	dst1, dst2 := "", []byte{3, 4, 5}
+
+	var str = "0123456789abcdef0123456789abcdef" // 32-byte hex string
+	var key [32]byte
+
+	// Convert the string to a byte slice
+	bSlice, err := hex.DecodeString(str)
+	if err != nil {
+		panic(err)
+	}
+
+	// Copy the byte slice into the array
+	copy(key[:], bSlice)
+	aesConfig := gotiny.NewAES256config(key)
+	data := gotiny.MarshalEncrypt(aesConfig, &src1, &src2)
+	gotiny.UnmarshalEncrypt(aesConfig, data, &dst1, &dst2)
 
 	fmt.Println(dst1 + string(dst2)) // print "hello world!"
 }
